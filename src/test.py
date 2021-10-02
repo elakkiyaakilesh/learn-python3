7eb6cde908e0bb1d9424abe710411631c545e6e4
elakkiya <elakkiyavenkatachalam@gmail.com>
2021-10-02 20:10:38 +0530

    added test_datatype method

--- src/test.py
+++ src/test.py
@@ -3,11 +3,16 @@ def test_sum():
     print("test_sum executed successfully")
 
 def test_sum_tuple():
-    assert sum((1, 2, 2)) == 6, "Should be 6"
+    assert sum((1, 2, 2)) == 5, "Should be 6"
     print("test_sum_tuple executed successfully")
-	
+
+def test_datatype():
+    assert (isinstance(5/3, float))
+    print("test_datatype executed successfully")
+
 if __name__ == "__main__":
     test_sum()
     test_sum_tuple()
+    test_datatype()
     print("Everything passed")
 
