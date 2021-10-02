cbfd46edfead3f5a424ee9d7e154a27a503348d3
elakkiya <elakkiyavenkatachalam@gmail.com>
2021-10-02 20:02:40 +0530

    Added negative test case

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ def test_sum():
     print("test_sum executed successfully")
 
 def test_sum_tuple():
-    assert sum((1, 2, 2)) == 5, "Should be 6"
+    assert sum((1, 2, 2)) == 6, "Should be 6"
     print("test_sum_tuple executed successfully")
 	
 if __name__ == "__main__":
